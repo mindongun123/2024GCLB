addfbf01adf4acd6a0f8f5be704a070b115abc63
Mai Phuong <nmaiphuong309@gmail.com>
2024-07-17 13:49:30 +0700

    commit checkout

--- Assets/Scripts/DataSheetExcel.cs
+++ Assets/Scripts/DataSheetExcel.cs
@@ -1,11 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-<<<<<<< HEAD
-=======
 using System.Data;
 using System.Linq;
 using System.Text.RegularExpressions;
->>>>>>> MPbranch
 using UnityEngine;
 
 public class DataSheetExcel : MonoBehaviour
